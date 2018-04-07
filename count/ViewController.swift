e86151ed983cfd8c139a7320ad6914584d4c22c9
sho <shocho0101@hotmail.co.jp>
2018-04-07 14:57:28 +0900

    背景画像を変更

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,45 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus(){
+        number += 1
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func multiply(){
+        number *= 2
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func divide(){
+        number /= 2
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        changeColor()
+    }
+    
+    func changeColor(){
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
     }
 
 
